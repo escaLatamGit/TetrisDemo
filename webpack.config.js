d8dafbe9bdc9cc6d233099f0aba3bad60e43acff
escaOG <carlos@other-guys.com>
2020-04-26 17:48:28 -0300

    📦 Creating Package and setting up babel and webpack config

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
+    ],
+    resolve: {
+        alias: {
+            '@model': path.resolve(__dirname, 'src/model/')
+        }
+    }
+};
