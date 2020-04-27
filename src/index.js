e2935fd335f5ac7d0ef3f3f5af62d84a61445fa9
Carlos <esca.latam@gmail.com>
2020-04-26 22:44:34 -0300

    ✨  Creating Tetris Pieces Model and Reference

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import {Game,BoardSize} from "@model/game.model";
 import "@styles/global.css";
+import {TetrisPiece,Location} from "@model/tetris-piece.model";
 const fps = 50;
-const sizes = new BoardSize(800,400);
+const pixel =10
+const sizes = new BoardSize(pixel*4*4*2,pixel*4*4,pixel,pixel);
 const gameInstance = new Game(fps,sizes);
 
 
 gameInstance.init("#game-container","#game-canvas");
+
+
+const piece = new TetrisPiece(gameInstance,'L','blue',new Location(0,0))
+
+piece.angle=3;
+piece.draw();
 
---
+++ src/model/tetris-piece.model.js
@@ -0,0 +1,87 @@
+import {Game} from "@model/game.model";
+import pieces from '@ref/tetris-pieces.reference';
+
+export class Location {
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
+
+export class TetrisPiece {
+
+    constructor(GameModel = new Game(), type = null, color = null, location = new Location()) {
+        this.game = GameModel;
+        this.type = type;
+        this.location = location;
+        this.angle = 0;
+        this.snapshots = [pieces[type]];
+        while (this.snapshots.length < 4) {
+            this.snapshots.push(this.rotatePiece(this.snapshots[this.snapshots.length - 1]))
+        }
+        this.color = color;
+    }
+
+    move(xInc = 0, yInc = 0) {
+        if (xInc)
+            this.location.x += xInc;
+        if (yInc)
+            this.location.y += yInc;
+
+    }
+
+    moveLeft(inc = 1) {
+
+    }
+
+    moveRight(inc = 1) {
+
+    }
+
+    moveUp(inc = 1) {
+
+    }
+
+    moveDown(inc = 1) {
+
+    }
+
+    draw() {
+        const pieceMap = this.snapshots[this.angle];
+        if (!pieceMap) throw new Error(`Invalid Piece type found ${this.type}`);
+
+        for (const y in pieceMap) {
+            for (const x in pieceMap[y]) {
+                if (!pieceMap[y][x]) continue;
+                this.game.ctx.fillStyle = this.color;
+                this.game.ctx.fillRect(
+                    (this.location.x + x) * this.game.sizes.pixelWidth,
+                    (this.location.y + y) * this.game.sizes.pixelHeight,
+                    this.game.sizes.pixelWidth,
+                    this.game.sizes.pixelHeight)
+
+            }
+        }
+    }
+
+    rotate(clockwise = true) {
+        let angle = this.angle + (clockwise ? 1 : -1);
+        if (angle < 0) angle = angle + 4;
+        this.angle = angle % 4;
+        return this.angle;
+    }
+
+    rotatePiece(pieceMap) {
+        const rotatedMap = [];
+        if (!pieceMap.length || !pieceMap[0].length) throw new Error(`Invalid Matrix found at ${JSON.stringify(pieceMap)}`);
+        rotatedMap.length = pieceMap[0].length;
+        for (const y in pieceMap) {
+            for (const x in pieceMap[y]) {
+                if (!rotatedMap[x]) rotatedMap[x] = [];
+                rotatedMap[x].unshift(pieceMap[y][x]);
+            }
+        }
+        return rotatedMap;
+    }
+}
