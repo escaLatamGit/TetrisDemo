8101e71a0fd25091a32363d5d14aefbfa8efa2e4
Carlos <esca.latam@gmail.com>
2020-04-26 21:40:25 -0300

    🚧 Creating method to update canvas

--- src/model/game.model.js
+++ src/model/game.model.js
@@ -1,9 +1,8 @@
 export class BoardSize {
     constructor(height = 700, width = 500, pixelHeight = 50, pixelWidth) {
-
-        this.height = height
-        this.width = width
-        this.pixelHeight = pixelHeight
+        this.height = height;
+        this.width = width;
+        this.pixelHeight = pixelHeight;
         this.pixelWidth = pixelWidth || pixelHeight
     }
 }
@@ -11,22 +10,35 @@ export class BoardSize {
 export class Game {
 
     constructor(fps, boardSize = new BoardSize()) {
-        this.fps = fps
-        this.canvas = null
-        this.ctx = null
+        this.fps = fps;
+        this.canvas = null;
+        this.ctx = null;
         this.sizes = boardSize
     }
 
     init(containerSelector, canvasSelector) {
 
-        const container = document.querySelector(containerSelector)
-        if (!container) throw new Error(`DOM Element Not Found ${containerSelector}`)
-        container.addEventListener('onLoad', () => {
-            const canvas = document.querySelector(canvasSelector)
-            if (!canvas) throw new Error(`DOM Element Not Found ${canvasSelector}`)
-            this.canvas = canvas
-            this.ctx = canvas.getContext('2d')
-            console.log('success')
+        const container = document.querySelector(containerSelector);
+        if (!container) throw new Error(`DOM Element Not Found ${containerSelector}`);
+
+        const canvas = document.querySelector(canvasSelector);
+        if (!canvas) throw new Error(`DOM Element Not Found ${canvasSelector}`);
+        this.canvas = canvas;
+        this.ctx = canvas.getContext('2d');
+        this.refresh();
+        setInterval(this.refresh, 1000 / this.fps)
+
+    }
+
+    refresh() {
+        this.updateCanvasStyle({height: this.sizes.height, width: this.sizes.width})
+    }
+
+    updateCanvasStyle(style) {
+        if (!this.canvas || !style) return;
+
+        Object.entries(style).forEach(([key, val]) => {
+            this.canvas[key] = val
         })
     }
 
