1c40e5642adf89fd8fc54637d0c76044c42f3453
Carlos <esca.latam@gmail.com>
2020-04-26 23:50:33 -0300

    ⚡ adding implementation of init method on Tetris Game

--- src/model/tetris.game-model.js
+++ src/model/tetris.game-model.js
@@ -1,9 +1,40 @@
 import {Game} from "@model/game.model";
+import {KeyBoardManager} from "@model/key-board-manager.model";
+import {Location, TetrisPiece} from "@model/tetris-piece.model";
 
 export class TetrisGame extends Game {
 
+    constructor(fps, sizes) {
+        super(fps, sizes);
+        this.currPiece = null;
+        this.keyListener = new KeyBoardManager()
+
+    }
 
     refresh() {
         super.refresh();
+    }
+
+    init(containerSelector, canvasSelector) {
+        super.init(containerSelector, canvasSelector);
+        super.attachToRefresh(() => this.currPiece && this.currPiece.draw());
+        this.currPiece = new TetrisPiece(this, 'L', 'purple', new Location(0, 0));
+        this.keyListener.init();
+        this.keyListener.addListener('keydown', 'ArrowUp', () => {
+            if (!this.currPiece) return;
+            this.currPiece.rotate();
+        });
+        this.keyListener.addListener('keydown', 'ArrowLeft', () => {
+            if (!this.currPiece) return;
+            this.currPiece.moveLeft(this.sizes.pixelWidth);
+        });
+        this.keyListener.addListener('keydown', 'ArrowDown', () => {
+            if (!this.currPiece) return;
+            this.currPiece.moveDown(this.sizes.pixelHeight/2);
+        });
+        this.keyListener.addListener('keydown', 'ArrowRight', () => {
+            if (!this.currPiece) return;
+            this.currPiece.moveRight(this.sizes.pixelWidth);
+        });
     }
 }
