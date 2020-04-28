366a49b9b8049ea40a0d99c6223625d951eb50a2
Carlos Esparragoza <esca.latam@gmail.com>
2020-04-27 23:28:18 -0300

    💄 updating Panel Size to make prittier grid

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import {BoardSize} from "@model/game.model";
 import "@styles/global.css";
-import {TetrisPiece,Location} from "@model/tetris-piece.model";
 import {TetrisGame} from "@model/tetris.game-model";
 const fps = 50;
-const pixel =10;
-const sizes = new BoardSize( 16*3,16,pixel,pixel);
+const pixel =20;
+const sizes = new BoardSize( 18,9,pixel,pixel);
 const gameInstance = new TetrisGame(fps,sizes);
 
 
