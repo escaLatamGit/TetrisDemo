fa393707629d784c1484a491aa7b8d30c2ac60b5
escaOG <carlos@other-guys.com>
2020-04-30 00:50:16 -0300

    🚧 adding font loader to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -55,12 +55,19 @@ module.exports = {
             {
                 test: /\.(png|svg|jpg|gif)$/i,
                 use: [
-                    'file-loader',
-                ],
-                options:{
-                outputPath: 'img/',
-                publicPath: 'img/'}
+                    {
+                        loader: 'file-loader'
+                    },
+                ]
             },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                use: [
+                    {
+                        loader: 'file-loader'
+                    },
+                ]
+            }
         ]
     },
     plugins: [
