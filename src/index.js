6e7cb2e37f73499279c5ac47526cc8c7489b1223
Carlos <esca.latam@gmail.com>
2020-04-30 13:46:45 -0300

    🚧 Working on Tetris Tv Structure

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import {TetrisGame} from "@model/tetris.game-model";
 import {pieceSize} from "@model/tetris-piece.model";
 
 const fps = 50;
-const BaseBlocks = 5;
-const HeightBlocks = 8;
-const pixel = Math.floor(window.innerHeight / (HeightBlocks * pieceSize) * 0.8);
+const BaseBlocks = 10;
+const HeightBlocks = 10;
+const pixel = Math.floor(window.innerHeight / (HeightBlocks * pieceSize) * 0.5);
 const sizes = new BoardSize(HeightBlocks * pieceSize, BaseBlocks * pieceSize, pixel, pixel);
 const gameInstance = new TetrisGame(fps, sizes);
 
