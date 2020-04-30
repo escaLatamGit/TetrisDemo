5d74e0962cd82503edf6baaf329733213fdd1f54
Carlos <esca.latam@gmail.com>
2020-04-30 09:11:15 -0300

    ⚡ adding container to center Game Component

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {pieceSize} from "@model/tetris-piece.model";
 const fps = 50;
 const BaseBlocks = 5;
 const HeightBlocks = 8;
-const pixel = Math.floor(window.innerHeight / (HeightBlocks * pieceSize) * 0.95);
+const pixel = Math.floor(window.innerHeight / (HeightBlocks * pieceSize) * 0.8);
 const sizes = new BoardSize(HeightBlocks * pieceSize, BaseBlocks * pieceSize, pixel, pixel);
 const gameInstance = new TetrisGame(fps, sizes);
 
