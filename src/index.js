dbf33ef6adde4c135a051ff67a23615c10d94ccd
escaOG <carlos@other-guys.com>
2020-05-03 19:19:10 -0300

    ⚡  creating event listener to resize canvas

--- src/index.js
+++ src/index.js
@@ -3,18 +3,19 @@ import "@style/global.scss";
 import {TetrisGame} from "@model/tetris.game-model";
 import {pieceSize} from "@model/tetris-piece.model";
 
-const fps = 50;
-const pixel = 10;
-const container = document.querySelector(".tv-screen");
-const width = Math.floor( (container.offsetWidth-40)/pieceSize/pixel)*pieceSize*pixel;
-const height = width;
-console.log(height / pixel, width / pixel);
-const sizes = new BoardSize(height / pixel, width / pixel, pixel, pixel);
-const gameInstance = new TetrisGame(fps, sizes);
-
-
-gameInstance.init("#game-container", "#game-canvas");
-
-
-
+const init = function () {
+    const fps = 50;
+    const pixel = 10;
+    const container = document.querySelector(".tv-screen");
+    const width = Math.floor((container.offsetWidth - 40) / pieceSize / pixel) * pieceSize * pixel;
+    const height = width;
+    console.log(height / pixel, width / pixel);
+    const sizes = new BoardSize(height / pixel, width / pixel, pixel, pixel);
+    const gameInstance = new TetrisGame(fps, sizes);
+    gameInstance.init("#game-container", "#game-canvas");
+};
+init();
+document.addEventListener('resize', function () {
+    init();
+});
 
