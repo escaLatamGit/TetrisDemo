8243878590b8e2aa3447dc0466054f61135ca926
escaOG <carlos@other-guys.com>
2020-05-03 19:27:37 -0300

    🔨 fixing canvas box margin

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import {pieceSize} from "@model/tetris-piece.model";
 
 const init = function () {
     const fps = 50;
-    const pixel = 10;
+    const pixel = 30;
     const container = document.querySelector(".tv-screen");
     const width = Math.floor((container.offsetWidth - 40) / pieceSize / pixel) * pieceSize * pixel;
     const height = width;
