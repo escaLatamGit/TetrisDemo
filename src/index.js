5fac3001bc46eda61c508eb1bd2a2460fb732b01
escaOG <carlos@other-guys.com>
2020-05-06 23:10:48 -0300

    ⚡ Adding ScoreChange Listener to update UI value

--- src/index.js
+++ src/index.js
@@ -7,24 +7,30 @@ const init = function () {
     const fps = 50;
     const pixel = 30;
     const container = document.querySelector(".tv-screen");
-    const width = Math.floor((container.offsetWidth - 40) / pieceSize / pixel) * pieceSize * pixel; 
+    const width = Math.floor((container.offsetWidth - 40) / pieceSize / pixel) * pieceSize * pixel;
     console.log(width / pixel, width / pixel);
     const sizes = new BoardSize(width / pixel, width / pixel, pixel, pixel);
     const gameInstance = new TetrisGame(fps, sizes);
-    gameInstance.init( "#game-canvas");
-    return gameInstance
+    gameInstance.init("#game-canvas");
+    const scoreListener = ({score}) => {
+        const scoreContainer = document.querySelector("#score-container");
+        scoreContainer.innerHTML = score;
+    };
+    gameInstance.addStateChangeListener(scoreListener)
+    scoreListener(gameInstance)
+    return gameInstance;
 };
 
 let instance = init();
 let resizeTrigger = null;
-let awaitResize = function(){
+let awaitResize = function () {
     if (resizeTrigger)
         clearTimeout(resizeTrigger)
-    resizeTrigger = setTimeout(()=>{
+    resizeTrigger = setTimeout(() => {
         resizeTrigger = null;
         instance.destroy();
         instance = init();
-    },200)
+    }, 200)
 }
 window.addEventListener('resize', function () {
     awaitResize();
