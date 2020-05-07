059fe392b0aac54a78cb5e5cdc0fb0ebb87e5dc7
Carlos <esca.latam@gmail.com>
2020-05-07 18:48:32 -0300

    🚧 working on on/off logic btn

--- src/index.js
+++ src/index.js
@@ -3,36 +3,60 @@ import "@style/global.scss";
 import {TetrisGame} from "@model/tetris.game-model";
 import {pieceSize} from "@model/tetris-piece.model";
 
-const init = function () {
-    const fps = 50;
-    const pixel = 30;
+let instance;
+let resizeTrigger;
+const fps = 50;
+const pixel = 30;
+
+const endListener = ({gameEnd} = {gameEnd: true}) => {
+    const btnContainer = document.querySelector("#start-btn");
+    btnContainer.innerHTML = gameEnd?'On':'Off';
+}
+
+const scoreListener = ({score}) => {
+    const scoreContainer = document.querySelector("#score-container");
+    scoreContainer.innerHTML = score;
+};
+
+const load = function () {
     const container = document.querySelector(".tv-screen");
     const width = Math.floor((container.offsetWidth - 40) / pieceSize / pixel) * pieceSize * pixel;
-    console.log(width / pixel, width / pixel);
     const sizes = new BoardSize(width / pixel, width / pixel, pixel, pixel);
-    const gameInstance = new TetrisGame(fps, sizes);
-    gameInstance.init("#game-canvas");
-    const scoreListener = ({score}) => {
-        const scoreContainer = document.querySelector("#score-container");
-        scoreContainer.innerHTML = score;
-    };
-    gameInstance.addStateChangeListener(scoreListener)
-    scoreListener(gameInstance)
+    const gameInstance = new TetrisGame("#game-canvas",fps, sizes);
+    gameInstance.addStateChangeListener(scoreListener);
+    gameInstance.addStateChangeListener(endListener);
+    scoreListener({score:'0000'});
+    endListener();
+    gameInstance.init();
+    instance = gameInstance;
+}
+
+
+const start = function () {
+    const container = document.querySelector(".tv-screen");
+    const width = Math.floor((container.offsetWidth - 40) / pieceSize / pixel) * pieceSize * pixel;
+    const sizes = new BoardSize(width / pixel, width / pixel, pixel, pixel);
+    const gameInstance = new TetrisGame("#game-canvas",fps, sizes);
+    gameInstance.addStateChangeListener(scoreListener);
+    gameInstance.addStateChangeListener(endListener);
+    scoreListener({score:'0000'});
+    endListener();
+    gameInstance.start();
     return gameInstance;
 };
 
-let instance = init();
-let resizeTrigger = null;
 let awaitResize = function () {
+    if (!instance) return;
     if (resizeTrigger)
         clearTimeout(resizeTrigger)
     resizeTrigger = setTimeout(() => {
         resizeTrigger = null;
         instance.destroy();
-        instance = init();
+        instance = start();
     }, 200)
 }
 window.addEventListener('resize', function () {
     awaitResize();
 });
 
+load();
